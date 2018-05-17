000422e9730271d416c2cd855d0bc4454ba6568e
stephanieiglesias <iglesiasst18@hanover.edu>
2018-05-17 14:06:45 -0400

    Create handler called onStartCommand to DelayedMessageService

--- app/src/main/java/edu/hanover/joke/DelayedMessageService.java
+++ app/src/main/java/edu/hanover/joke/DelayedMessageService.java
@@ -2,14 +2,22 @@ package edu.hanover.joke;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.util.Log;
+import android.os.Handler;
+import android.widget.Toast;
 
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
+    private Handler handler;
 
     public DelayedMessageService() {
         super("DelayedMessageService");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler = new Handler();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -26,6 +34,12 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(final String text) {
-        Log.v("DelayedMessageService", "The message is: " + text);
-    }
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+            }
+        });
+        }
 }
+
